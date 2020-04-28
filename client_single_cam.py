a79ee7ce493e03eaf858c7bcaadd68c8d9612660
wanggor <anggorow9@gmail.com>
2020-04-28 10:36:50 +0700

    delete client

--- client_single_cam.py
+++ client_single_cam.py
@@ -9,18 +9,23 @@ from imutils.video import VideoStream, FileVideoStream
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--id", type=str,
-    help="name of CCTV", default="1")
+    help="CCTV ID", default="1")
 ap.add_argument("-s", "--source", type=str,
-    help="name source", default="None")
+    help="Video source", default="0")
 ap.add_argument("-n", "--name", type=str,
-    help="name tempat", default="My CCTV")
+    help="CCTV name", default="My CCTV")
 ap.add_argument("-u", "--url", type=str, 
-    help="height of image output", default="None")
+    help="Host server", default="None")
 ap.add_argument("-p", "--port", type=str, 
-    help="height of image output", default="5555")
+    help="Port server", default="5555")
 ap.add_argument("-a", "--analitic", type=str, 
-    help="image processing", default="true")
-
+    help="Image processing", default="true")
+ap.add_argument("-H", "--height", type=int, 
+    help="Height image", default=480)
+ap.add_argument("-W", "--width", type=int, 
+    help="Width image", default=640)
+ap.add_argument("-t", "--type", type=str, 
+    help="Type of client", default="single")
 args = vars(ap.parse_args())
 
 time.sleep(3.0)
@@ -30,16 +35,24 @@ if args["url"] == "None":
     sender  = imagezmq.ImageSender(connect_to=url,REQ_REP = True)
 else:
     sender  = imagezmq.ImageSender(connect_to=f'tcp://{args["url"]}:{args["port"]}', REQ_REP = True)
+
 source  = args["source"]
 id      = args["id"]
 name    = args["name"]
+type_cam    = args["type"]
 calibrate = "OK"
+
+process_opsi = ["rgb", "hsv", "grey"]
+opsi_active = "rgb"
 
 if source != "None":
     if source == "pi":
         vs = VideoStream(usePiCamera=True).start()
     else:
-        vs = VideoStream(int(source)).start()
+        if source.isnumeric():
+            vs = VideoStream(int(source)).start()
+        else:
+            vs = VideoStream(source).start()
 time.sleep(2.0)
 
 while True:
@@ -47,10 +60,17 @@ while True:
         frame1 = vs.read()
     else:
         frame1 = np.ones((480,640,3), np.uint8) * random.randint(1,255)
+    frame1 = cv2.resize(frame1,(args["width"], args["height"]))
 
-    data1 = {"id" : id, "name": name, "type":"primary", "process" :args["analitic"]}
+    if opsi_active == "hsv":
+        frame1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2HSV)
+    if opsi_active == "grey":
+        frame1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+    
+    data1 = {"id" : id, "name": name, "type":"primary", "process" :args["analitic"], "type_cam" : type_cam, "process_opsi" : process_opsi, "opsi_active":opsi_active}
+
     
     if frame1 is not None:
         respon = (sender.send_image_reqrep(data1, frame1))
-    time.sleep(0.05)
+        print(respon)
 vs.stop()
--- running.sh
+++ running.sh
@@ -1,1 +1,1 @@
-python3 client_single_cam.py -u localhost -i 0 -n 'Not Thermal Camera' -s pi
+python3 /root/wahyu_coba/stream-client/client_single_cam.py -u 192.168.1.12 -i 0 -n 'Pi Camera' -s pi -W 640 -H 480
