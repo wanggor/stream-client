95342027e44107d0c00adc88ec24cc03f7a67ee6
wanggor <anggorow9@gmail.com>
2020-04-29 10:48:51 +0700

    add option

--- client_single_cam.py
+++ client_single_cam.py
@@ -60,7 +60,7 @@ while True:
         frame1 = vs.read()
     else:
         frame1 = np.ones((480,640,3), np.uint8) * random.randint(1,255)
-    # frame1 = cv2.resize(frame1,(args["width"], args["height"]))
+    frame1 = cv2.resize(frame1,(args["width"], args["height"]))
 
     if opsi_active == "hsv":
         frame1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2HSV)
@@ -71,7 +71,8 @@ while True:
 
     
     if frame1 is not None:
-        respon = (sender.send_image_reqrep(data1, frame1))
-        opsi_active = respon.decode("utf-8").split("|")[1].split("#")[1]
+        respon = (sender.send_image_reqrep(data1, frame1)).decode("utf-8").split("|")
+        if len(respon) > 1:
+            opsi_active = respon[1].split("#")[1]
         
 vs.stop()
